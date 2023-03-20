8cda41c82dafe32583eb7d9bafe9502ab0d35bb4
devsibong <115763000+devsibong@users.noreply.github.com>
2023-03-20 15:35:51 +0900

    [Bronze V] Title: 아스키 코드, Time: 212 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String c = scanner.nextLine();
+		char temp = c.charAt(0);
+		System.out.println((int)temp);
+	}
+}
